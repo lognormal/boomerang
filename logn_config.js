523722f2ff51d29ab3ee49cce6d2473b2c6257c4
Philip Tellis <philip.tellis@gmail.com>
2011-10-31 00:30:29 -0400

    lognormal configurator

---
+++ logn_config.js
@@ -0,0 +1,9 @@
+(function() {
+var s0=document.getElementsByTagName("script")[0],
+    s1=document.createElement("script");
+
+s1.type="text/javascript";
+s1.src="http://lognormal.net/boomerang/config.js?key=%client_apikey%";
+
+s0.parentNode.insertBefore(s1, s0);
+}());
