d36b87f04a8a6a9831476718b5f3f81f8fbb90ad
Philip Tellis <philip.tellis@gmail.com>
2011-11-08 18:13:49 -0500

    add document domain to config request since referrer may not always show up

--- logn_config.js
+++ logn_config.js
@@ -1,9 +1,10 @@
 (function() {
 var s0=document.getElementsByTagName("script")[0],
-    s1=document.createElement("script");
+    s1=document.createElement("script"),
+    d=document.URL.replace(/^https?:\/\/(.*?)\/.*$/, '$1');
 
 s1.type="text/javascript";
-s1.src="http://lognormal.net/boomerang/config.js?key=%client_apikey%";
+s1.src="http://lognormal.net/boomerang/config.js?key=%client_apikey%&d=" + encodeURIComponent(d);
 
 s0.parentNode.insertBefore(s1, s0);
 }());
