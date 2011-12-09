80adbab5ed707210fe185050236fe8bd0963ac87
Philip Tellis <philip.tellis@gmail.com>
2011-12-10 00:01:14 +0530

    work with scheme relative urls so that ssl is not a problem

--- logn_config.js
+++ logn_config.js
@@ -4,7 +4,7 @@ var s0=document.getElementsByTagName("script")[0],
     d=document.URL.replace(/^https?:\/\/(.*?)\/.*$/, '$1');
 
 s1.type="text/javascript";
-s1.src="http://lognormal.net/boomerang/config.js?key=%client_apikey%&d=" + encodeURIComponent(d);
+s1.src="//lognormal.net/boomerang/config.js?key=%client_apikey%&d=" + encodeURIComponent(d);
 
 s0.parentNode.insertBefore(s1, s0);
 }());
