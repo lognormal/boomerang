162d2707225c26ef6b8eacb9a6ff2fe5e2980f85
Philip Tellis <philip.tellis@gmail.com>
2012-02-24 18:14:03 -0500

    make it a little more compact

--- logn_config.js
+++ logn_config.js
@@ -1,10 +1,9 @@
-(function() {
-var s0=document.getElementsByTagName("script")[0],
-    s1=document.createElement("script"),
-    d=document.URL.replace(/^https?:\/\/(.*?)\/.*$/, '$1');
+(function(doc, s) {
+var s0=doc.getElementsByTagName(s)[0],
+    s1=doc.createElement(s),
+    d=doc.URL.replace(/^https?:\/\/(.*?)\/.*$/, '$1');
 
-s1.type="text/javascript";
 s1.src="//lognormal.net/boomerang/config.js?key=%client_apikey%&d=" + encodeURIComponent(d);
 
 s0.parentNode.insertBefore(s1, s0);
-}());
+}(document, "script"));
