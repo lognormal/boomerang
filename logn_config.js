4168f8e47035b333cf2d08d21d7a620b0cd8eeee
Philip Tellis <philip.tellis@gmail.com>
2012-03-16 10:13:01 -0400

    measure time it takes to load config.js

--- logn_config.js
+++ logn_config.js
@@ -6,6 +6,8 @@ var s0=doc.getElementsByTagName(s)[0],
 if(d.match(/^mhtml/))
 	return;
 
+s1.onload=function() { BOOMR.plugins.RT.endTimer('t_configjs') };
+BOOMR.plugins.RT.startTimer('t_configjs');
 s1.src="//lognormal.net/boomerang/config.js?key=%client_apikey%&d=" + encodeURIComponent(d);
 
 s0.parentNode.insertBefore(s1, s0);
