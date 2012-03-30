919cdc8c565715299f7fa20e8526cc0ae23ae140
Philip Tellis <philip.tellis@gmail.com>
2012-03-30 15:38:58 -0400

    Load config.js after onload
    We turn logn_config.js into a full plugin so that we can attach to the
    page_ready event and load config.js after that fires.  This makes sure
    that page load does not block waiting for config.js to load up.  In most
    cases config.js loads up fast, but there may be cases where TCP connect
    takes a long time and page load blocks on this.
    
    Note that we cannot send the beacon until config.js has loaded because
    it contains the crumb.  It will also call init() a second time with new
    configs.

--- logn_config.js
+++ logn_config.js
@@ -1,14 +1,46 @@
-(function(doc, s) {
-var s0=doc.getElementsByTagName(s)[0],
-    s1=doc.createElement(s),
-    d=doc.URL.replace(/^https?:\/\//, '').replace(/[:?;\/].*$/, '');
+(function(w) {
+var d=w.document,
+    s="script",
+    dom=d.URL.replace(/^https?:\/\//, '').replace(/[:?;\/].*$/, ''),
+    complete = false;
 
-if(d.match(/^mhtml/))
+// Don't even bother creating the plugin if this is mhtml
+if(d.match(/^mhtml/) || d.match(/^file:\//)) {
 	return;
+}
 
-s1.onload=function() { BOOMR.plugins.RT.endTimer('t_configjs') };
-BOOMR.plugins.RT.startTimer('t_configjs');
-s1.src="//lognormal.net/boomerang/config.js?key=%client_apikey%&d=" + encodeURIComponent(d);
+var loaded=function() {
+	BOOMR.plugins.RT.endTimer('t_configjs');
+	complete=true;
+	BOOMR.sendBeacon();
+};
 
-s0.parentNode.insertBefore(s1, s0);
-}(document, "script"));
+var load=function() {
+	var s0=d.getElementsByTagName(s)[0],
+	    s1=d.createElement(s),
+
+	s1.onload=loaded;
+	BOOMR.plugins.RT.startTimer('t_configjs');
+	s1.src="//lognormal.net/boomerang/config.js?key=%client_apikey%&d=" + encodeURIComponent(d);
+
+	s0.parentNode.insertBefore(s1, s0);
+	s0=s1=null;
+};
+
+BOOMR.plugins.LOGN = {
+	init: function() {
+		if(complete) {
+			return this;
+		}
+
+		BOOMR.subscribe("page_ready", load, null, null);
+
+		return this;
+	},
+
+	is_complete: function() {
+		return complete;
+	}
+};
+
+}(window));
