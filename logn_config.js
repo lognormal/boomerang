d16bfc661ddf65b0efceaef5b7cd6229451343d3
Philip Tellis <philip.tellis@gmail.com>
2012-05-23 04:09:22 -0400

    add boomerang version to config.js query string

--- logn_config.js
+++ logn_config.js
@@ -26,6 +26,7 @@ var load=function() {
 	t_start=new Date().getTime();
 	s1.src="//lognormal.net/boomerang/config.js?key=%client_apikey%&d=" + encodeURIComponent(dom)
 		+ '&t=' + Math.round(t_start/(5*60*1000))	// add time field at 5 minute resolution so that we force a cache bust if the browser's being nasty
+		+ '&v=' + BOOMR.version				// boomerang version so we can force a reload for old versions
 		+ (complete?"&r=":"");				// if this is running after complete, then we're just refreshing the crumb
 
 	s0.parentNode.insertBefore(s1, s0);
