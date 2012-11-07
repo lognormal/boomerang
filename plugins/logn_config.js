94a20be52c4289cd79e746362261a87a58b09e72
Philip Tellis <philip.tellis@gmail.com>
2012-11-07 00:00:50 -0800

    Tell config.js if we're in an iframe
    config.js needs to look at the right frame for config variables, so it
    needs to know if boomerang is in the main window or an iframe.  Since
    boomerang knows this, it should pass this information back to config.js

--- plugins/logn_config.js
+++ plugins/logn_config.js
@@ -27,6 +27,7 @@ var load=function() {
 	s1.src="//lognormal.net/boomerang/config.js?key=%client_apikey%&d=" + encodeURIComponent(dom)
 		+ '&t=' + Math.round(t_start/(5*60*1000))	// add time field at 5 minute resolution so that we force a cache bust if the browser's being nasty
 		+ '&v=' + BOOMR.version				// boomerang version so we can force a reload for old versions
+		+ (w == window?"":"&if=")			// if this is running in an iframe, we need to look for config vars in parent window
 		+ (complete?"&r=":"");				// if this is running after complete, then we're just refreshing the crumb
 
 	s0.parentNode.insertBefore(s1, s0);
