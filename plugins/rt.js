7095e5b02b7d96ba309190203138c74b1d05e36d
Philip Tellis <philip@lognormal.com>
2013-02-06 01:46:38 -0800

    Read and set cookie every time init is called
    The first time init is called, site_domain may have been null, so we
    wouldn't be able to set cookies.  The second time init is called,
    initialized is true, so the setCookie call is never executed.  This
    causes sessions to break.
    
    We move the cookie checking and setting code up before the initialized
    check.  This is all the code that can and should run on every call to
    init... not just because it needs the latest data, but also because
    config may change the cookie name it needs to read/set.

--- plugins/rt.js
+++ plugins/rt.js
@@ -289,8 +289,14 @@ BOOMR.plugins.RT = {
 		BOOMR.utils.pluginConfig(impl, config, "RT",
 					["cookie", "cookie_exp", "session_exp", "strict_referrer"]);
 
-		// only initialize once.  we still collect config every time init is called, but we set
-		// event handlers only once
+		impl.initFromCookie(true);
+		if(!impl.sessionStart) {
+			impl.sessionStart = BOOMR.t_lstart || BOOMR.t_start;
+		}
+		impl.setCookie(null, false);
+
+		// only initialize once.  we still collect config and check/set cookies
+		// every time init is called, but we attach event handlers only once
 		if(impl.initialized) {
 			return this;
 		}
@@ -329,12 +335,6 @@ BOOMR.plugins.RT = {
 		// it manually with their own timers.  It may not always contain a referrer
 		// (eg: XHR calls).  We set default values for these cases
 		impl.r = impl.r2 = d.referrer.replace(/#.*/, '');
-
-		impl.initFromCookie(true);
-		if(!impl.sessionStart) {
-			impl.sessionStart = BOOMR.t_lstart || BOOMR.t_start;
-		}
-		impl.setCookie(null, false);
 
 		impl.initialized = true;
 		return this;
