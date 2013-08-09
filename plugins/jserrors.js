0dc9cca102463bcef7d432ce9aa9284bc2ac846c
abarre <a@fasterize.com>
2013-08-09 13:48:18 +0200

    Add jserrors.js

---
+++ plugins/jserrors.js
@@ -0,0 +1,38 @@
+/**
+\file jserrors.js
+Count js errors for current page
+*/
+
+(function(w) {
+
+  BOOMR = BOOMR || {};
+  BOOMR.plugins = BOOMR.plugins || {};
+
+  var complete, jserrors = 0;
+
+  function done() {
+    BOOMR.addVar('jserrors', jserrors);
+    complete = true;
+    BOOMR.sendBeacon();
+  }
+
+  function iscomplete() {
+    return complete;
+  }
+
+  function newError() {
+    jserrors++;
+  }
+
+  BOOMR.plugins.JsErrors = {
+    init: function() {
+      BOOMR.utils.addListener(window, 'error', newError);
+      BOOMR.subscribe("page_ready", done);
+
+      return this;
+    },
+
+    is_complete: iscomplete
+  };
+
+}(window));
