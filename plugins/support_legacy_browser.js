7035279513164dee0c712810075cc91b05d4a342
abarre <a@fasterize.com>
2013-08-13 18:38:04 +0200

    Fix legacy plugin.

--- plugins/support_legacy_browser.js
+++ plugins/support_legacy_browser.js
@@ -11,18 +11,13 @@ Send the variable t_page for browsers that don't support navtiming.
   var complete;
 
   function iscomplete() {
-    return complete;
-  }
-
-  function ready() {
-    BOOMR.plugins.RT.startTimer('t_page', w.BOOMR_lstart);
-    complete = true;
-    BOOMR.sendBeacon();
+    return true;
   }
 
   BOOMR.plugins.Legacy = {
     init: function() {
-      BOOMR.subscribe("page_ready", ready);
+      BOOMR.plugins.RT.startTimer('t_page', w.BOOMR_lstart);
+      BOOMR.sendBeacon();
       return this;
     },
 
