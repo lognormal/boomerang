f6ae5ae2cf6799c11046f986d4b3910580787ae0
abarre <a@fasterize.com>
2013-08-13 17:25:35 +0200

    Add support for legacy browser that don't have navtiming.

---
+++ plugins/support_legacy_browser.js
@@ -0,0 +1,32 @@
+/**
+\file support_legacy_browser.js
+Send the variable t_page for browsers that don't support navtiming.
+*/
+
+(function(w, d) {
+
+  BOOMR = BOOMR || {};
+  BOOMR.plugins = BOOMR.plugins || {};
+
+  var complete;
+
+  function iscomplete() {
+    return complete;
+  }
+
+  function ready() {
+    BOOMR.plugins.RT.startTimer('t_page', w.BOOMR_lstart);
+    complete = true;
+    BOOMR.sendBeacon();
+  }
+
+  BOOMR.plugins.Domstats = {
+    init: function() {
+      BOOMR.subscribe("page_ready", ready);
+      return this;
+    },
+
+    is_complete: iscomplete
+  };
+
+}(this, document));
