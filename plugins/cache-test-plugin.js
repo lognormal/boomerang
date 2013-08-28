e8f75b076492a7bd74d3d4ad0873c0078b28f544
Philip Tellis <philip@lognormal.com>
2013-08-28 10:11:41 -0700

    let plugin track how long it took for cache-test to load. small times hint at something loaded from cache

--- plugins/cache-test-plugin.js
+++ plugins/cache-test-plugin.js
@@ -5,35 +5,29 @@ BOOMR.plugins = BOOMR.plugins || {};
 var dc=document,
     s="script",
     dom=location.hostname,
-    complete=false;
+    complete=false,
+    t_start;
 
 // Don't even bother creating the plugin if this is mhtml
 if(!dom || dom == 'localhost' || dom.match(/\.\d+$/) || dom.match(/^mhtml/) || dom.match(/^file:\//)) {
 	return;
 }
 
-var loaded=function() {
-	if(complete) {
-		return;
-	}
-	complete = true;
-	BOOMR.sendBeacon();
-};
-
 var load=function() {
 	var s0=dc.getElementsByTagName(s)[0],
 	    s1=dc.createElement(s);
 
-	s1.onload = loaded;
-	s1.src="//lognormal.net/boomerang/cache-test.js";
-	BOOMR.addVar('cch.ce', new Date().getTime());
+	s1.onload = BOOMR.plugins.CT.loaded;
+	s1.src="//lognormal.net/boomerang/cache-test-v2.js";
+	t_start = new Date().getTime();
+	BOOMR.addVar('cch.ce', t_start));
 
 	s0.parentNode.insertBefore(s1, s0);
 	s0=s1=null;
 
 	// this is a timeout so we don't wait forever to send the beacon
 	// if the server fails
-	setTimeout(loaded, 500);
+	setTimeout(BOOMR.plugins.CT.loaded, 500);
 };
 
 BOOMR.plugins.CT = {
@@ -54,7 +48,22 @@ BOOMR.plugins.CT = {
 
 	is_complete: function() {
 		return complete;
+	},
+
+	loaded: function(t) {
+		if(complete) {
+			return;
+		}
+		if(!t) t=-1
+		// how long did it take for the call to return
+		BOOMR.addVar({
+			'cch.lt': new Date().getTime()-t_start,
+			'cch.se': t
+		});
+		complete = true;
+		BOOMR.sendBeacon();
 	}
+
 };
 
 }());
