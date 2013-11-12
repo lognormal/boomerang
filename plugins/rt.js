b122a85ce7dca51377b4e3e14e0cd81471aecfc7
Philip Tellis <philip@lognormal.com>
2013-11-11 20:54:27 -0500

    Add more comments to make code more understandable

--- plugins/rt.js
+++ plugins/rt.js
@@ -119,11 +119,20 @@ impl = {
 		subcookies.s = Math.max(+subcookies.ul||0, +subcookies.cl||0);
 
 		BOOMR.debug("Read from cookie " + BOOMR.utils.objectToString(subcookies), "rt");
+
+		// If we have a start time, and either a referrer, or a clicked on URL,
+		// we check if the start time is usable
 		if(update_start && subcookies.s && (subcookies.r || subcookies.nu)) {
 			this.r = subcookies.r;
 			url = BOOMR.utils.hashQueryString(d.URL, true);
 
+			// Either the URL of the page setting the cookie needs to match document.referrer
 			BOOMR.debug(this.r + " =?= " + this.r2, "rt");
+
+			// Or the start timer was no more than 15ms after a click or form submit
+			// and the URL clicked or submitted to matches the current page's URL
+			// (note the start timer may be later than click if both click and beforeunload fired
+			// on the previous page)
 			BOOMR.debug(subcookies.s + " <? " + (+subcookies.cl+15), "rt");
 			BOOMR.debug(subcookies.nu + " =?= " + url, "rt");
 
@@ -131,11 +140,17 @@ impl = {
 					( subcookies.s < +subcookies.cl + 15 && subcookies.nu === url )
 			) {
 				this.t_start = subcookies.s;
+
+				// additionally, if we have a pagehide, or unload event, that's a proxy
+				// for the first byte of the current page, so use that wisely
 				if(+subcookies.hd > subcookies.s) {
 					this.t_fb_approx = parseInt(subcookies.hd, 10);
 				}
 			}
 		}
+
+		// regardless of whether the start time was usable or not, it's the last action that
+		// we measured, so use that for the session
 		if(subcookies.s) {
 			this.lastActionTime = subcookies.s;
 		}
@@ -312,6 +327,9 @@ BOOMR.plugins.RT = {
 		BOOMR.utils.pluginConfig(impl, config, "RT",
 					["cookie", "cookie_exp", "session_exp", "strict_referrer"]);
 
+		// Now pull out start time information and session information from the cookie
+		// We'll do this every time init is called, and every time we call it, it will
+		// overwrite values already set (provided there are values to read out)
 		impl.initFromCookie(true);
 		if(!BOOMR.session.start) {
 			BOOMR.session.start = BOOMR.t_lstart || BOOMR.t_start;
