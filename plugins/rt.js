689e8f5882f7d49d3e8c9cf45b941f2ff1691c9a
Philip Tellis <philip@lognormal.com>
2013-11-12 16:16:51 -0800

    set referrer only on unload

--- plugins/rt.js
+++ plugins/rt.js
@@ -66,7 +66,7 @@ impl = {
 					}
 				}
 				else {
-					if (k==="nu") {
+					if (k==="nu" || k==="r") {
 						params[k] = BOOMR.utils.hashQueryString(params[k], true);
 					}
 
@@ -74,10 +74,6 @@ impl = {
 				}
 			}
 		}
-
-		// We use document.URL instead of location.href because of a bug in safari 4
-		// where location.href is URL decoded
-		subcookies.r = BOOMR.utils.hashQueryString(d.URL, true);
 
 		subcookies.dm = BOOMR.session.domain;
 		subcookies.si = BOOMR.session.ID;
@@ -348,7 +344,10 @@ impl = {
 		}
 
 		// set cookie for next page
-		this.updateCookie(edata.type === 'beforeunload'?'ul':'hd');
+		// We use document.URL instead of location.href because of a bug in safari 4
+		// where location.href is URL decoded
+		this.updateCookie(edata.type === 'beforeunload'?'ul':'hd', { 'r': d.URL } );
+
 
 		this.unloadfired = true;
 	},
