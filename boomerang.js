dad4fbbd1ecb16d0e802980a083267ed7eb65568
Philip Tellis <philip@lognormal.com>
2013-11-26 17:40:53 -0500

    restrict max length of boomerang cookies to 400 characters instead of 4000 to prevent overflow

--- boomerang.js
+++ boomerang.js
@@ -204,7 +204,7 @@ boomr = {
 				c.push("expires=" + exp);
 			}
 
-			if ( nameval.length < 4000 ) {
+			if ( nameval.length < 400 ) {
 				d.cookie = c.join('; ');
 				// confirm cookie was set (could be blocked by user's settings, etc.)
 				savedval = this.getCookie(name);
