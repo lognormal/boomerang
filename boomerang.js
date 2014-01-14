fc008cd8f23fc8ee5203f8549505eb075756087e
Philip Tellis <philip@lognormal.com>
2014-01-13 17:18:23 -0800

    bump size to 500

--- boomerang.js
+++ boomerang.js
@@ -205,7 +205,7 @@ boomr = {
 				c.push("expires=" + exp);
 			}
 
-			if ( nameval.length < 400 ) {
+			if ( nameval.length < 500 ) {
 				d.cookie = c.join('; ');
 				// confirm cookie was set (could be blocked by user's settings, etc.)
 				savedval = this.getCookie(name);
