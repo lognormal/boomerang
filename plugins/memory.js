dca01d103e11eb2de8421474ed3553fa9d51606e
Philip Tellis <philip@lognormal.com>
2014-12-02 10:06:07 -0500

    Refactor to be more efficient
    - No need to check capabilities on every call, check once and cache
    - Add number of domains from resource timing if available
    - Run on before_beacon instead of on page_ready because we should collect
      a new snapshot for every beacon that is sent when it is sent

--- plugins/memory.js
+++ plugins/memory.js
@@ -9,7 +9,7 @@ see: http://code.google.com/p/chromium/issues/detail?id=43281
 */
 
 (function() {
-
+var w, p={}, d, f, m;
 // First make sure BOOMR is actually defined.  It's possible that your plugin is loaded before boomerang, in which case
 // you'll need this.
 BOOMR = BOOMR || {};
@@ -20,9 +20,51 @@ if (BOOMR.plugins.Memory) {
 
 // A private object to encapsulate all your implementation details
 var impl = {
-	complete: false,
 	done: function() {
-		var w, d, fn, p, c, m, f;
+		var res, doms={}, a;
+		// If we have resource timing, get number of resources
+		if(p.getEntriesByType && p.getEntriesByType("resource").length) {
+			res = p.getEntriesByType("resource");
+			BOOMR.addVar("dom.res", res.length);
+
+			a = document.createElement("a");
+
+			res.forEach(function(r) {
+				a.href=r.name;
+				doms[a.hostname] = true;
+			});
+
+			BOOMR.addVar("dom.doms", Object.keys(doms).length);
+		}
+		else {
+			BOOMR.removeVar("dom.res");
+		}
+
+		if(m) {
+			BOOMR.addVar({
+				"mem.total": m.totalJSHeapSize,
+				"mem.used" : m.usedJSHeapSize
+			});
+		}
+
+		// handle IE6/7 weirdness regarding host objects
+		// See: http://stackoverflow.com/questions/7125288/what-is-document-getelementbyid
+		if (f) {
+			BOOMR.addVar({
+				"dom.ln": f.call(d, "*").length,
+				"dom.sz": f.call(d, "html")[0].innerHTML.length,
+				"dom.img": f.call(d, "img").length,
+				"dom.script": f.call(d, "script").length
+			});
+		}
+
+		// no need of sendBeacon because we're called when the beacon is being sent
+	}
+};
+
+BOOMR.plugins.Memory = {
+	init: function() {
+		var fn, c;
 
 		try {
 			w  = BOOMR.window;
@@ -35,49 +77,22 @@ var impl = {
 			BOOMR.addError(err, "Memory.done");
 		}
 
-		m = (p && p.memory ? p.memory : (c && c.memory ? c.memory : null));
-
-		// If we have resource timing, get number of resources
-		if(p && p.getEntries && p.getEntries().length) {
-			BOOMR.addVar("dom.res", p.getEntries().length);
-		}
-
-		if(m) {
-			BOOMR.addVar({
-				"mem.total": m.totalJSHeapSize,
-				"mem.used" : m.usedJSHeapSize
-			});
-		}
-
 		// handle IE6/7 weirdness regarding host objects
 		// See: http://stackoverflow.com/questions/7125288/what-is-document-getelementbyid
 		if (fn) {
 			f  = (fn.call === undefined ? function(tag) { return fn(tag); } : fn);
-
-			BOOMR.addVar({
-				"dom.ln": f.call(d, "*").length,
-				"dom.sz": f.call(d, "html")[0].innerHTML.length,
-				"dom.img": f.call(d, "img").length,
-				"dom.script": f.call(d, "script").length
-			});
 		}
 
-		this.complete = true;
-		BOOMR.sendBeacon();
-	}
-};
+		m = (p && p.memory ? p.memory : (c && c.memory ? c.memory : null));
 
-BOOMR.plugins.Memory = {
-	init: function() {
-		// we do this on onload so that we take a memory and dom snapshot after most things have run
-		BOOMR.subscribe("page_ready", impl.done, null, impl);
-		BOOMR.subscribe("xhr_load", impl.done, null, impl);
-		BOOMR.subscribe("page_unload", impl.done, null, impl);
+		// we do this before sending a beacon to get the snapshot when the beacon is sent
+		BOOMR.subscribe("before_beacon", impl.done, null, impl);
 		return this;
 	},
 
 	is_complete: function() {
-		return impl.complete;
+		// Always true since we run on before_beacon, which happens after the check
+		return true;
 	}
 };
 
