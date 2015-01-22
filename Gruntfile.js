9860d975f80835ca93b395c5c3d8fdfaa801326e
Nic Jansma <nic@nicj.net>
2015-01-22 15:07:51 -0500

    tests/library -> tests/unit

--- Gruntfile.js
+++ Gruntfile.js
@@ -187,7 +187,8 @@ module.exports = function (grunt) {
                     "tests/vendor/mocha/mocha.js",
                     "tests/vendor/chai/chai.js",
                     "tests/vendor/expect/index.js",
-                    "tests/library/*.js",
+                    "tests/unit/*.js",
+                    "tests/integration/*.js",
                     "./build/<%= pkg.name %>-<%= pkg.releaseVersion %>.<%= buildDate %>.js"
                 ]
             },
