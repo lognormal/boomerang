a83ef9c5de1a400a00f38bf68b0baf9f75285ddb
Nic Jansma <nic@nicj.net>
2015-02-23 17:44:49 -0500

    E2E page-params test

---
+++ tests/pages/03-page-params/00-custom-metrics.js
@@ -0,0 +1,11 @@
+/*eslint-env mocha*/
+/*global BOOMR_test,assert*/
+
+describe("e2e/03-page-params/00-custom-metrics", function() {
+	var tf = BOOMR.plugins.TestFramework;
+
+	it("Should find XPath Currency value", function() {
+		assert.isString(tf.lastBeaconData["cmet.XpathCurrency"]);
+		assert.equal("143.12", tf.lastBeaconData["cmet.XpathCurrency"]);
+	});
+});
