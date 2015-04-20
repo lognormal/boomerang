108bcd6f8083355ad02c593315300c89fc04029a
Nic Jansma <nic@nicj.net>
2015-04-20 13:44:41 -0400

    Custom Dimensions test for URLQueryParam type

--- tests/page-templates/04-page-params/04-custom-dimensions-cookies.js
+++ tests/page-templates/04-page-params/04-custom-dimensions-cookies.js
@@ -5,6 +5,9 @@ describe("e2e/04-page-params/04-custom-dimensions-cookies", function() {
     var tf = BOOMR.plugins.TestFramework;
     var t = BOOMR_test;
 
+    var dom = location.hostname;
+    var onDomain = !(!dom || dom === "localhost" || dom.match(/\.\d+$/) || dom.match(/^mhtml/) || dom.match(/^file:\//));
+
     it("Should pass basic beacon validation", function(done) {
         t.validateBeaconWasSent(done);
     });
@@ -12,5 +15,19 @@ describe("e2e/04-page-params/04-custom-dimensions-cookies", function() {
     it("Should be missing custom dimension 1 - Cookie", function() {
         var b = tf.lastBeacon();
         assert.equal(b["cdim.CD1"], undefined);
+    });
+
+    it("Should be have custom dimension 2 - Cookie - If running on a domain", function() {
+        if (onDomain) {
+            var b = tf.lastBeacon();
+            assert.equal(b["cdim.CD2"], "true");
+        }
+    });
+
+    it("Should be missing custom dimension 2 - Cookie - If running on localhost or an IP", function() {
+        if (!onDomain) {
+            var b = tf.lastBeacon();
+            assert.equal(b["cdim.CD2"], undefined);
+        }
     });
 });
