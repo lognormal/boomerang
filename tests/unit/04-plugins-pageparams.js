0e0c7b77c10990a824692442d40265fe8ba43404
Nic Jansma <nic@nicj.net>
2015-04-20 13:12:12 -0400

    tests/unit/04-page-params: Don't test stuff here (since it depends on page load), test in E2E tests

--- tests/unit/04-plugins-pageparams.js
+++ tests/unit/04-plugins-pageparams.js
@@ -17,48 +17,4 @@ describe("BOOMR.plugins.PageParams", function() {
             assert.isTrue(BOOMR.plugins.PageParams.is_complete());
         });
     });
-
-    describe("basic tests", function() {
-        it("Should pass a basic test", function() {
-            var test = this;
-            test.run_assertions = function(vars) {
-                this.beaconVars = vars;
-                this.resume();
-            };
-
-            // 1. Define a global variable
-            window.PageGroupVariable = "PageGroup";
-
-            // 2. Tell PageParams to look for a basic JavaScript variable
-            BOOMR.init({
-                autorun: false,
-                PageParams: {
-                    pageGroups: [
-                        {
-                            parameter1: "PageGroupVariable"
-                        }
-                    ]
-                },
-                log: null
-            });
-
-            // 3. Subscribe to the before_beacon method so we can tell if our params are available
-            BOOMR.subscribe(
-                "before_beacon",
-                test.run_assertions,
-                null,
-                test
-            );
-
-            // 4. We'll now fire the page ready event so that boomerang runs the code
-            BOOMR.page_ready();
-
-            setTimeout(function() {
-                assert.isTrue(BOOMR.plugins.PageParams.is_complete());
-
-                assert.isTrue(test.beaconVars.hasOwnProperty("h.pg"));
-                assert.strictEqual("PageGroup", test.beaconVars["h.pg"]);
-            }, 1000);
-        });
-    });
 });
