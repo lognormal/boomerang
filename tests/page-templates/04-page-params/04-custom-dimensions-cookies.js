5169c8018cd0f418ff1956fbcb1aedea8a028dae
Nic Jansma <nic@nicj.net>
2015-04-22 22:45:24 -0400

    e2e/04-page-params/04-custom-dimensions-cookies: Test if a cookie can be set to determine which branch to test, as PhantomJS can set cookies on localhost

--- tests/page-templates/04-page-params/04-custom-dimensions-cookies.js
+++ tests/page-templates/04-page-params/04-custom-dimensions-cookies.js
@@ -5,8 +5,11 @@ describe("e2e/04-page-params/04-custom-dimensions-cookies", function() {
     var tf = BOOMR.plugins.TestFramework;
     var t = BOOMR_test;
 
-    var dom = location.hostname;
-    var onDomain = !(!dom || dom === "localhost" || dom.match(/\.\d+$/) || dom.match(/^mhtml/) || dom.match(/^file:\//));
+    // set a cookie
+    document.cookie = ["test_cookie=true", "path=/", "domain=" + location.hostname].join("; ");
+
+    // determine if it was set OK
+    var cookieSetOK = (" " + document.cookie + ";").indexOf(" test_cookie=") !== -1;
 
     it("Should pass basic beacon validation", function(done) {
         t.validateBeaconWasSent(done);
@@ -17,15 +20,15 @@ describe("e2e/04-page-params/04-custom-dimensions-cookies", function() {
         assert.equal(b["cdim.CD1"], undefined);
     });
 
-    it("Should be have custom dimension 2 - Cookie - If running on a domain", function() {
-        if (onDomain) {
+    it("Should be have custom dimension 2 - Cookie - If cookies can be set (on a domain or in PhantomJS)", function() {
+        if (cookieSetOK) {
             var b = tf.lastBeacon();
             assert.equal(b["cdim.CD2"], "true");
         }
     });
 
-    it("Should be missing custom dimension 2 - Cookie - If running on localhost or an IP", function() {
-        if (!onDomain) {
+    it("Should be missing custom dimension 2 - Cookie - If cookies can't be set (on localhost or an IP)", function() {
+        if (!cookieSetOK) {
             var b = tf.lastBeacon();
             assert.equal(b["cdim.CD2"], undefined);
         }
