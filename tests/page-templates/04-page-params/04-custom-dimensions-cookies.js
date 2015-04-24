53a7639e2265fa1d877f4e799a2c21973a15ad9d
Nic Jansma <nic@nicj.net>
2015-04-24 14:18:04 -0400

    Added BOOMR_test.canSetCookie()

--- tests/page-templates/04-page-params/04-custom-dimensions-cookies.js
+++ tests/page-templates/04-page-params/04-custom-dimensions-cookies.js
@@ -5,12 +5,6 @@ describe("e2e/04-page-params/04-custom-dimensions-cookies", function() {
     var tf = BOOMR.plugins.TestFramework;
     var t = BOOMR_test;
 
-    // set a cookie
-    document.cookie = ["test_cookie=true", "path=/", "domain=" + location.hostname].join("; ");
-
-    // determine if it was set OK
-    var cookieSetOK = (" " + document.cookie + ";").indexOf(" test_cookie=") !== -1;
-
     it("Should pass basic beacon validation", function(done) {
         t.validateBeaconWasSent(done);
     });
@@ -21,14 +15,14 @@ describe("e2e/04-page-params/04-custom-dimensions-cookies", function() {
     });
 
     it("Should be have custom dimension 2 - Cookie - If cookies can be set (on a domain or in PhantomJS)", function() {
-        if (cookieSetOK) {
+        if (t.canSetCookies()) {
             var b = tf.lastBeacon();
             assert.equal(b["cdim.CD2"], "true");
         }
     });
 
     it("Should be missing custom dimension 2 - Cookie - If cookies can't be set (on localhost or an IP)", function() {
-        if (!cookieSetOK) {
+        if (!t.canSetCookies()) {
             var b = tf.lastBeacon();
             assert.equal(b["cdim.CD2"], undefined);
         }
