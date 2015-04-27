d045c9e716cc3605d3795b4d2166dbdc7afa870b
Charles Vazac <cvazac@soasta.com>
2015-04-27 11:44:00 -0700

    more lint

--- tests/page-templates/06-bugs/92560.js
+++ tests/page-templates/06-bugs/92560.js
@@ -4,8 +4,7 @@
 describe("e2e/06-bugs/92560", function() {
     var tf = BOOMR.plugins.TestFramework;
 
-    it("Should get 2 beacons: 1 onload, 1 xhr (2nd xhr should be excluded)", function()
-    {
-      assert.equal(tf.beaconCount(), 2);
+    it("Should get 2 beacons: 1 onload, 1 xhr (2nd xhr should be excluded)", function() {
+        assert.equal(tf.beaconCount(), 2);
     });
 });
