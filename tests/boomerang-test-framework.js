098017d0c18f0d7db68158d7a6fbbe4519b35085
Charles Vazac <cvazac@soasta.com>
2015-05-06 16:44:35 -0700

    revert change to 'tests/boomerang-test-framework.js'

--- tests/boomerang-test-framework.js
+++ tests/boomerang-test-framework.js
@@ -22,8 +22,6 @@
         page_ready: function() {
             this.fired_page_ready = true;
         },
-        before_beacon: function(data) {
-        },
         onbeacon: function(data) {
             this.beacons.push(_.clone(data));
             this.fired_onbeacon = true;
@@ -42,12 +40,11 @@
             this.fired_before_unload = true;
         },
         init: function() {
-            if(this.initialized) {
+            if (this.initialized) {
                 return this;
             }
 
             BOOMR.subscribe("page_ready", this.page_ready, null, this);
-            BOOMR.subscribe("before_beacon", this.before_beacon, null, this);
             BOOMR.subscribe("onbeacon", this.onbeacon, null, this);
             BOOMR.subscribe("before_unload", this.before_unload, null, this);
 
@@ -164,7 +161,8 @@
                 window.document.addEventListener("onBoomerangLoaded", function() {
                     t.init(config);
                 });
-            } else if (window.document.attachEvent) {
+            }
+            else if (window.document.attachEvent) {
                 window.document.attachEvent("onBoomerangLoaded", function() {
                     t.init(config);
                 });
@@ -188,7 +186,7 @@
             "h.d": "localhost",
             "h.t": new Date().getTime(),
             "h.cr": "abc"
-        })
+        });
 
         // setup Mocha
         window.mocha.globals(["BOOMR", "PageGroupVariable"]);
@@ -205,13 +203,14 @@
             }
 
             BOOMR.subscribe("onbeacon", function() {
-                if (++beaconsSeen == config.testAfterOnBeacon) {
+                if (++beaconsSeen === config.testAfterOnBeacon) {
                     // wait a few more ms so the beacon fires
                     // TODO: Trim this timing down if we can make it more reliable
                     setTimeout(t.runTests, 1000);
                 }
             });
-        } else {
+        }
+        else {
             BOOMR.setImmediate(t.runTests);
         }
 
@@ -274,7 +273,17 @@
         assert.isString(tf.lastBeacon().v);
 
         done();
-    }
+    };
+
+    t.canSetCookies = function() {
+        var testCookieName = "test_cookie";
+
+        // set a cookie
+        document.cookie = [testCookieName + "=true", "path=/", "domain=" + location.hostname].join("; ");
+
+        // determine if it was set OK
+        return (" " + document.cookie + ";").indexOf(" " + testCookieName + "=") !== -1;
+    };
 
     window.BOOMR_test = t;
 
