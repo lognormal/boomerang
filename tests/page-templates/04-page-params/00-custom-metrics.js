28056b99a804840214bdd608d5a05ad44e66214e
Nic Jansma <nic@nicj.net>
2015-05-11 16:20:45 -0400

    IE XPath parser: Allow single or double quotes

--- tests/page-templates/04-page-params/00-custom-metrics.js
+++ tests/page-templates/04-page-params/00-custom-metrics.js
@@ -24,7 +24,7 @@ describe("e2e/04-page-params/00-custom-metrics", function() {
         assert.equal(b.cmet3, undefined);
     });
 
-    it("Should have the custom metric 4 - XPath", function() {
+    it("Should have the custom metric 4 - XPath with ID (single quote)", function() {
         var b = tf.lastBeacon();
         assert.equal(b.cmet4, 444.44);
     });
@@ -32,5 +32,15 @@ describe("e2e/04-page-params/00-custom-metrics", function() {
     it("Should have the custom metric 5 - URL", function() {
         var b = tf.lastBeacon();
         assert.equal(b.cmet5, 1);
+    });
+
+    it("Should have the custom metric 6 - XPath with ID (double quote)", function() {
+        var b = tf.lastBeacon();
+        assert.equal(b.cmet6, 444.44);
+    });
+
+    it("Should have the custom metric 7 - XPath rooted at an ID (double quote) with another element name following", function() {
+        var b = tf.lastBeacon();
+        assert.equal(b.cmet7, 444.44);
     });
 });
