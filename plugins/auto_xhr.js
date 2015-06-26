8aa4c38169b439d896c7d58558946cbf37c9d1ee
Nic Jansma <nic@nicj.net>
2015-06-26 14:03:01 -0400

    Only don't orig_send if we threw an exception in orig_open; Move all XHR errors to constants

--- plugins/auto_xhr.js
+++ plugins/auto_xhr.js
@@ -7,6 +7,12 @@
 	// Default SPA activity timeout, in milliseconds
 	var SPA_TIMEOUT = 1000;
 
+	// Custom XHR status codes
+	var XHR_STATUS_TIMEOUT        = -1001;
+	var XHR_STATUS_ABORT          = -999;
+	var XHR_STATUS_ERROR          = -998;
+	var XHR_STATUS_OPEN_EXCEPTION = -997;
+
 	// If this browser cannot support XHR, we'll just skip this plugin which will
 	// save us some execution time.
 
@@ -698,9 +704,9 @@
 				}
 
 				addListener("load");
-				addListener("timeout", -1001);
-				addListener("error",    -998);
-				addListener("abort",    -999);
+				addListener("timeout", XHR_STATUS_TIMEOUT);
+				addListener("error",   XHR_STATUS_ERROR);
+				addListener("abort",   XHR_STATUS_ABORT);
 
 				resource.url = a.href;
 				resource.method = method;
@@ -719,7 +725,7 @@
 				catch (e) {
 					// if there was an exception during .open(), .send() won't work either,
 					// so let's fire loadFinished now
-					resource.status = -997;
+					resource.status = XHR_STATUS_OPEN_EXCEPTION;
 					loadFinished();
 				}
 			};
@@ -729,7 +735,8 @@
 
 				// call the original send method unless there was an error
 				// during .open
-				if (typeof resource.status === "undefined") {
+				if (typeof resource.status === "undefined" ||
+				    resource.status !== XHR_STATUS_OPEN_EXCEPTION) {
 					return orig_send.apply(req, arguments);
 				}
 			};
