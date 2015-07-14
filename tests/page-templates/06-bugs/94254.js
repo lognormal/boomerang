4c61cfedabbed04923d9184750b7798dcba27f86
Andreas Marschke <andreas.marschke@gmail.com>
2015-07-15 00:46:38 +0200

    Add E2E tests for memory leak fix

---
+++ tests/page-templates/06-bugs/94254.js
@@ -0,0 +1,31 @@
+/*eslint-env mocha*/
+/*global assert*/
+
+describe("e2e/06-bugs/94254", function() {
+	var tf = BOOMR.plugins.TestFramework;
+	var assert = chai.assert;
+
+	it("Should send a beacon", function(){
+		assert.isTrue(tf.fired_onbeacon);
+	});
+
+	it("Should not have an element in the iframe", function(done){
+		var observer = new MutationObserver(function(mutations) {
+			assert.isArray(mutations);
+			assert.lengthOf(mutations, 2);
+
+			assert.lengthOf(mutations[0].addedNodes, 1);
+			assert.lengthOf(mutations[1].removedNodes, 1);
+
+			assert.strictEqual(mutations[0].addedNodes[0].nodeName, "SCRIPT");
+			assert.strictEqual(mutations[1].removedNodes[0].nodeName, "SCRIPT");
+
+			assert.include(mutations[0].addedNodes[0].src, "boomerang.test");
+			assert.include(mutations[1].removedNodes[0].src, "boomerang.test");
+
+			observer.disconnect();
+			done();
+		});
+		observer.observe(document.head, { attributes: true, childList: true, characterData: false });
+	});
+});
