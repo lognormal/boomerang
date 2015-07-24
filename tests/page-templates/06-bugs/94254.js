43307236918c78971c88f24883d9214882bd102d
Andreas Marschke <andreas.marschke@gmail.com>
2015-07-24 23:54:02 +0200

    Make tests for 94254 work in phantomjs and in e2e environment

--- tests/page-templates/06-bugs/94254.js
+++ tests/page-templates/06-bugs/94254.js
@@ -1,16 +1,21 @@
 /*eslint-env mocha*/
-/*global assert*/
+/*global assert,runTests*/
 
 describe("e2e/06-bugs/94254", function() {
 	var tf = BOOMR.plugins.TestFramework;
 
-	it("Should send a beacon", function(){
-		assert.isTrue(tf.fired_onbeacon);
+	it("Should send a beacon", function(done){
+		if (runTests() && window.MutationObserver) {
+			assert.isTrue(tf.fired_onbeacon);
+			done();
+		}
+		else {
+			done();
+		}
 	});
 
 	it("Should not have an element in the iframe", function(done){
-		if (window.MutationObserver && typeof BOOMR.plugins.RT.navigationStart() !== "undefined") {
-
+		if (window.MutationObserver && runTests()) {
 			var observer = new MutationObserver(function(mutations) {
 				assert.isArray(mutations);
 				assert.lengthOf(mutations, 2);
@@ -21,8 +26,8 @@ describe("e2e/06-bugs/94254", function() {
 				assert.strictEqual(mutations[0].addedNodes[0].nodeName, "SCRIPT");
 				assert.strictEqual(mutations[1].removedNodes[0].nodeName, "SCRIPT");
 
-				assert.include(mutations[0].addedNodes[0].src, "boomerang.test");
-				assert.include(mutations[1].removedNodes[0].src, "boomerang.test");
+				assert.include(mutations[0].addedNodes[0].src, document.location.hostname);
+				assert.include(mutations[1].removedNodes[0].src, document.location.hostname);
 
 				observer.disconnect();
 				done();
