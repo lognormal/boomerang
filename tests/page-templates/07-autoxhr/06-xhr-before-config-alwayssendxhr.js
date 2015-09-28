ff49961b006f56e3db02dbfcb56b4279fe901b52
Charles Vazac <cvazac@soasta.com>
2015-09-28 16:12:09 -0700

    update to tests

--- tests/page-templates/07-autoxhr/06-xhr-before-config-alwayssendxhr.js
+++ tests/page-templates/07-autoxhr/06-xhr-before-config-alwayssendxhr.js
@@ -5,11 +5,11 @@ describe("05-xhr-before-onload-alwayssendxhr", function() {
 	var t = BOOMR_test;
 	var tf = BOOMR.plugins.TestFramework;
 
-	it("Should get 5 beacons: 1 onload, 4 xhr (XMLHttpRequest !== null)", function(done) {
+	it("Should get 4 beacons: 1 onload, 3 xhr (XMLHttpRequest !== null)", function(done) {
 		t.ifAutoXHR(
 			done,
 			function() {
-				assert.lengthOf(tf.beacons, 5);
+				assert.lengthOf(tf.beacons, 4);
 				done();
 			});
 	});
@@ -18,35 +18,27 @@ describe("05-xhr-before-onload-alwayssendxhr", function() {
 		var beacon = tf.beacons[0];
 		assert.equal(beacon["http.initiator"], "xhr");
 		assert.equal(beacon["rt.start"], "manual");
-		assert.include(beacon.u, "boomerang-latest-debug.js&1");
+		assert.include(beacon.u, "boomerang-latest-debug.js&xhr1");
 	});
 
-	it("Second beacon should be an XHR, opened and completed after config, before onload", function() {
+	it("Second beacon should be the navigation beacon", function() {
 		var beacon = tf.beacons[1];
-		assert.equal(beacon["http.initiator"], "xhr");
-		assert.equal(beacon["rt.start"], "manual");
-		assert.include(beacon.u, "boomerang-latest-debug.js&2");
-	});
-
-	it("Third beacon should be the navigation beacon", function() {
-		var beacon = tf.beacons[2];
 		assert.isUndefined(beacon["http.initiator"]);
 		assert.equal(beacon["rt.start"], "navigation");
 	});
 
-	it("Fourth beacon should be an XHR, opened before config, completed after config, after onload", function() {
+	it("Third beacon should be an XHR, opened before config, completed after config", function() {
+		var beacon = tf.beacons[2];
+		assert.equal(beacon["http.initiator"], "xhr");
+		assert.equal(beacon["rt.start"], "manual");
+		assert.include(beacon.u, "boomerang-latest-debug.js&xhr2");
+	});
+
+	it("Fourth beacon should be an XHR, opened and completed after config", function() {
 		var beacon = tf.beacons[3];
 		assert.equal(beacon["http.initiator"], "xhr");
 		assert.equal(beacon["rt.start"], "manual");
-		assert.include(beacon.u, "boomerang-latest-debug.js&3");
-		assert.isTrue(beacon.t_done > 2000);
-	});
-
-	it("Fifth beacon should be an XHR, opened and completed after config, after onload", function() {
-		var beacon = tf.beacons[4];
-		assert.equal(beacon["http.initiator"], "xhr");
-		assert.equal(beacon["rt.start"], "manual");
-		assert.include(beacon.u, "boomerang-latest-debug.js&4");
+		assert.include(beacon.u, "boomerang-latest-debug.js&xhr3");
 	});
 
 });
