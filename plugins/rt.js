f68344438b6fb5f313532d96c49dce84163b79a3
Nic Jansma <nic@nicj.net>
2016-05-12 10:49:22 -0400

    RT: Protect against null window/document (#378)

--- plugins/rt.js
+++ plugins/rt.js
@@ -909,6 +909,12 @@
 			if (w !== BOOMR.window) {
 				w = BOOMR.window;
 			}
+
+			// protect against undefined window/document
+			if (!w || !w.document) {
+				return;
+			}
+
 			d = w.document;
 
 			BOOMR.utils.pluginConfig(impl, config, "RT",
