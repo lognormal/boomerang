7c93d3868a41f5868a0b16eb988c49ded773094f
Nic Jansma <nic@nicj.net>
2017-02-10 17:40:55 -0500

    SPA navigations: Remove timers from hard nav

--- plugins/rt.js
+++ plugins/rt.js
@@ -895,7 +895,7 @@
 
 			if (ename === "load" ||
 			    ename === "visible" ||
-				(ename === "xhr" && edata && BOOMR.utils.inArray(edata.initiator, BOOMR.constants.BEACON_TYPE_SPAS))) {
+				(ename === "xhr" && edata && edata.initiator === "spa_hard")) {
 				// Only add Boomerang timings to page load and SPA beacons
 				impl.getBoomerangTimings();
 			}
