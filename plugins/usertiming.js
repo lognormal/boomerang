79da25196b360d8333ce76a165c758bc938a121c
Nigel Heron <nheron@querymetrics.om>
2017-04-28 00:13:05 -0400

    Recheck for UserTiming support on page_ready event

--- plugins/usertiming.js
+++ plugins/usertiming.js
@@ -49,7 +49,6 @@ UserTimingCompression must be loaded before this plugin's init is called.
 			BOOMR.removeVar("usertiming");
 			r = this.getUserTiming();
 			if (r) {
-				BOOMR.info("User Timing API entries found since the last beacon", "usertiming");
 				BOOMR.addVar({
 					"usertiming": r
 				});
@@ -63,42 +62,61 @@ UserTimingCompression must be loaded before this plugin's init is called.
 				BOOMR.removeVar("usertiming");
 			}
 			this.complete = false;
-		}
-	};
+		},
 
-	BOOMR.plugins.UserTiming = {
-		init: function() {
-			var p = BOOMR.window.performance;
+		subscribe: function() {
+			BOOMR.subscribe("before_beacon", this.addEntriesToBeacon, null, this);
+			BOOMR.subscribe("onbeacon", this.clearMetrics, null, this);
+		},
 
-			if (impl.initialized) {
-				return this;
+		pageReady: function() {
+			if (this.checkSupport()) {
+				this.subscribe();
+			}
+		},
+
+		checkSupport: function() {
+			if (this.supported) {
+				return true;
 			}
 
+			var p = BOOMR.getPerformance();
 			// Check that we have getEntriesByType and that the required UserTimingCompression library is available
-			if (p && typeof p.getEntriesByType === "function" && typeof UserTimingCompression !== "undefined") {
+			if (p && typeof p.getEntriesByType === "function" &&
+			    typeof UserTimingCompression !== "undefined") {
 				var marks = p.getEntriesByType("mark");
 				var measures = p.getEntriesByType("measure");
 				// Check that the results of getEntriesByType for marks and measures are Arrays
 				// Some polyfill libraries may incorrectly implement this
-				if (Object.prototype.toString.call(marks) === "[object Array]" && Object.prototype.toString.call(measures) === "[object Array]") {
+				if (Object.prototype.toString.call(marks) === "[object Array]" &&
+				    Object.prototype.toString.call(measures) === "[object Array]") {
 					BOOMR.info("Client supports User Timing API", "usertiming");
-
-					BOOMR.subscribe("before_beacon", impl.addEntriesToBeacon, null, impl);
-					BOOMR.subscribe("onbeacon", impl.clearMetrics, null, impl);
-
-					impl.supported = true;
+					this.supported = true;
+					return true;
 				}
 			}
+			return false;
+		}
+	};
+
+	BOOMR.plugins.UserTiming = {
+		init: function(config) {
+			if (impl.initialized) {
+				return this;
+			}
+
+			if (impl.checkSupport()) {
+				impl.subscribe();
+			}
 			else {
-				impl.complete = true;
+				// usertiming isn't supported by the browser. Let's check again when the page is ready to see if a polyfill was loaded.
+				BOOMR.subscribe("page_ready", impl.pageReady, null, impl);
 			}
 
 			impl.initialized = true;
-
 			return this;
 		},
 		is_complete: function() {
-			// After initialization, this plugin is ready to beacon data
 			return true;
 		},
 		is_supported: function() {
