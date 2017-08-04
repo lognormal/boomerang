2eeb7eca476e6ab5c7bd86dc62bf456bafde84c3
Nic Jansma <nic@nicj.net>
2017-08-04 13:57:24 -0400

    Revert "Add a few NavigationTiming2 fields if available" (#645)

--- tests/page-templates/06-bugs/92542.js
+++ tests/page-templates/06-bugs/92542.js
@@ -5,6 +5,6 @@ describe("e2e/06-bugs/92542", function() {
 	var tf = BOOMR.plugins.TestFramework;
 
 	it("Should not have any errors", function() {
-		assert.isUndefined(tf.lastBeacon().errors);
+		assert.equal(typeof tf.lastBeacon().errors, "undefined");
 	});
 });
