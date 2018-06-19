321a5659e62a08e8f29bebb364063f250e3df9be
Nigel Heron <nheron@akamai.com>
2018-06-19 12:53:41 -0400

    Protect access to localStorage variable

---
+++ tests/page-templates/13-logn/07-localStorage-error.js
@@ -0,0 +1,37 @@
+/*eslint-env mocha*/
+/*global BOOMR_test*/
+
+describe("e2e/13-logn/07-localstorage-error", function() {
+	var tf = BOOMR.plugins.TestFramework;
+	var t = BOOMR_test;
+
+	it("Should have sent one beacon", function(done) {
+		this.timeout(10000);
+		t.ensureBeaconCount(done,  1);
+	});
+
+	it("Should have a h.cr from server", function() {
+		var b = tf.lastBeacon();
+		assert.notEqual(b["h.cr"], "abc");
+	});
+
+	it("Should have a h.pg of FROMSERVER", function() {
+		var b = tf.lastBeacon();
+		assert.equal(b["h.pg"], "FROMSERVER");
+	});
+
+	it("Should have a config load time on beacon", function() {
+		var b = tf.lastBeacon();
+		assert.operator(parseInt(b.t_configjs), ">=", 2000, "t_configjs is above 2000 ms");
+	});
+
+	it("Should have a config first byte time on beacon", function() {
+		var b = tf.lastBeacon();
+		assert.operator(parseInt(b.t_configfb), ">=", 2000, "t_configfb is above 2000 ms");
+	});
+
+	it("Should not have a localStorage config load time on beacon", function() {
+		var b = tf.lastBeacon();
+		assert.isUndefined(b.t_configls);
+	});
+});
